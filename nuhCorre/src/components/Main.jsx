0c87fffe51f39f3347ea162ae94c088a5a7e1c1f
najort1 <calopsita443@gmail.com>
2024-09-27 15:02:02 -0300

    Main content
    ainda sem CSSS

--- nuhCorre/src/components/Main.jsx
+++ nuhCorre/src/components/Main.jsx
@@ -1,3 +1,68 @@
-const PesquisaVaga = () = {
-    
-}
+import { useState } from "react";
+
+const PesquisaVaga = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSearch = (event) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+    console.log(`Pesquisando por: ${value}`);
+  };
+
+  return (
+    <>
+      <div className="tudo">
+        <div className="container">
+          <div className="titulo">
+            <h1>Encontre as Melhores Vagas de Emprego</h1>
+            <h2>Temos mais de 320 mil oportunidades para você</h2>
+          </div>
+          <div className="pesquisa">
+            <select onChange={handleSearch} value={selectedOption}>
+              <option value="">Tipo</option>
+              <option value="vagas">Vagas</option>
+              <option value="empresas">Empresas</option>
+              <option value="curriculos">Currículos</option>
+            </select>
+
+            <input type="text" placeholder="Pesquisar" />
+            <select name="Cidade" id="selecionarCidade">
+                <option value="">Cidade</option>
+                <option value="saoPaulo">São Paulo</option>
+                <option value="rioDeJaneiro">Rio de Janeiro</option>
+                <option value="beloHorizonte">Belo Horizonte</option>
+                <option value="brasilia">Brasília</option>
+                <option value="salvador">Salvador</option>
+                <option value="curitiba">Curitiba</option>
+                <option value="fortaleza">Fortaleza</option>
+                <option value="manaus">Manaus</option>
+                <option value="goiania">Goiânia</option>
+                <option value="belem">Belém</option>
+                <option value="portoAlegre">Porto Alegre</option>
+                <option value="recife">Recife</option>
+                <option value="campinas">Campinas</option>
+                <option value="saoLuis">São Luís</option>
+                <option value="maceio">Maceió</option>
+                <option value="teresina">Teresina</option>
+                <option value="natal">Natal</option>
+                <option value="joaoPessoa">João Pessoa</option>
+                <option value="aracaju">Aracaju</option>
+                <option value="cuiaba">Cuiabá</option>
+                <option value="campoGrande">Campo Grande</option>
+                <option value="palmas">Palmas</option>
+                <option value="rioBranco">Rio Branco</option>
+                <option value="boaVista">Boa Vista</option>
+                <option value="macapa">Macapá</option>
+                <option value="portoVelho">Porto Velho</option>
+                <option value="rioBranco">Rio Branco</option>
+            </select>
+            <button>Pesquisar</button>
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PesquisaVaga;
