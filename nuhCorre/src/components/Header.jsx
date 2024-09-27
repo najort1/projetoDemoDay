8de552a24a86032a2d0617e96eb721862bb90731
Luana <luana_dark34@hotmail.com>
2024-09-27 15:55:17 -0300

    Html

---
+++ nuhCorre/src/components/Header.jsx
@@ -0,0 +1,30 @@
+export function Header(){
+
+    return(
+
+        <header>
+
+            <div id='logo'>
+                
+                NuhCorre
+
+            </div>
+            <div>
+
+                <ul>
+
+                    <li>Sou candidato</li>
+                    <li>Sou empresa</li>
+
+                </ul>
+                
+                <button>Entrar</button>
+                <button>Cadastro</button>
+
+            </div>
+
+        </header>
+
+    )
+
+}
