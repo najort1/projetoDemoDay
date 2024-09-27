6fe0f84472d3d84a9ba0e2483b33f937e4f3c5ff
najort1 <calopsita443@gmail.com>
2024-09-27 17:26:57 -0300

    Adição footer

---
+++ nuhCorre/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import './styleFooter.css'
+export function Footer(){
+
+    return(
+
+        <footer>
+
+            <div id="logo">
+
+                NuhCorre
+
+            </div>
+
+            <ul>
+
+                <li id='cima'>Sobre</li>
+                <li id='cima'>Serviços</li>
+                <li id='cima'>Suporte</li>
+
+            </ul>
+
+            <ul>
+
+                <li>
+
+                    <img width="30" height="30" src="https://img.icons8.com/ffffff/50/facebook-new.png" alt="facebook-new"/>
+
+                </li>
+                <li>
+                    <img width="30" height="30" src="https://img.icons8.com/ffffff/material-outlined/24/instagram-new--v1.png" alt="instagram-new--v1"/>
+                </li>
+                <li>
+
+                    <img width="30" height="30" src="https://img.icons8.com/ffffff/ios-glyphs/30/linkedin.png" alt="linkedin"/>
+                    
+                </li>
+
+            </ul>
+
+        </footer>
+
+    )
+
+}
