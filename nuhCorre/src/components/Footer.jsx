442a5f291032563f81f77ca20dd9741a46490fef
najort1 <calopsita443@gmail.com>
2024-10-02 13:54:21 -0300

    correções + adição css Luana
    correções de responsividade

--- nuhCorre/src/components/Footer.jsx
+++ nuhCorre/src/components/Footer.jsx
@@ -6,31 +6,39 @@ function Footer(){
 
         <footer>
 
-            <ul>
+            <div className="linksERedes">
 
-                <li id='cima'>Sobre</li>
-                <li id='cima'>Serviços</li>
-                <li id='cima'>Suporte</li>
+                    <div className="listaDeLinks">
+                    <ul className='listaLinks'>
+                        <li id='cima'>Sobre</li>
+                        <li id='cima'>Serviços</li>
+                        <li id='cima'>Suporte</li>
+                    </ul>
+                    </div>
+    
+ 
+                    <div className="listaDeRedesSociais">
+                        <ul className='listaRedesSociais'>
+        
+                            <li>
+        
+                                <img width="30" height="30" src="https://img.icons8.com/ffffff/50/facebook-new.png" alt="facebook-new"/>
+        
+                            </li>
+                            <li>
+                                <img width="30" height="30" src="https://img.icons8.com/ffffff/material-outlined/24/instagram-new--v1.png" alt="instagram-new--v1"/>
+                            </li>
+                            <li>
+        
+                                <img width="30" height="30" src="https://img.icons8.com/ffffff/ios-glyphs/30/linkedin.png" alt="linkedin"/>
+                                
+                            </li>
+        
+                        </ul>
+                    </div>
+    
 
-            </ul>
-
-            <ul>
-
-                <li>
-
-                    <img width="30" height="30" src="https://img.icons8.com/ffffff/50/facebook-new.png" alt="facebook-new"/>
-
-                </li>
-                <li>
-                    <img width="30" height="30" src="https://img.icons8.com/ffffff/material-outlined/24/instagram-new--v1.png" alt="instagram-new--v1"/>
-                </li>
-                <li>
-
-                    <img width="30" height="30" src="https://img.icons8.com/ffffff/ios-glyphs/30/linkedin.png" alt="linkedin"/>
-                    
-                </li>
-
-            </ul>
+            </div>
 
         </footer>
 
--- nuhCorre/src/components/Header.jsx
+++ nuhCorre/src/components/Header.jsx
@@ -1,35 +1,29 @@
-import './styleHeader.css'
+import "./styleHeader.css";
 
-function Header(){
+function Header() {
+  return (
+    <header>
+      <div id="logo">NuhCorre</div>
 
-    return(
+      <div className="conteudoHeader">
+        <div className="listaIdentificador">
+          <ul className="identificadorCandidato">
+            <li>
+              <a href="">Sou candidato</a>
+            </li>
+            <li>
+              <a href="">Sou empresa</a>
+            </li>
+          </ul>
+        </div>
 
-        <header>
-
-            <div id='logo'>
-
-                NuhCorre
-
-            </div>
-
-            
-
-                <ul>
-                    <li><a href="">Sou candidato</a></li>
-                    <li><a href="">Sou empresa</a></li>
-                </ul>
-
-                <div className="botoes">
-                    <button className="btn">Entrar</button>
-                    <button className="btn">Cadastro</button>
-                </div>
-            
-
-
-        </header>
-
-    )
-
+        <div className="botoes">
+          <button className="btn">Entrar</button>
+          <button className="btn">Cadastro</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
