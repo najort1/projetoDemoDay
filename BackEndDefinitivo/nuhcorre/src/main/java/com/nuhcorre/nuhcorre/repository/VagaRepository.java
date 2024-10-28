5ebf314db5c9ab278da85f07be5d26c938b3ee61
najort1 <calopsita443@gmail.com>
2024-10-28 19:30:18 -0300

    Querys
    eu ia começar a fazer mas aconteceu uma coisa que preciso resolveu previsão de conclusão da api em 3 dias

--- BackEndDefinitivo/nuhcorre/src/main/java/com/nuhcorre/nuhcorre/repository/VagaRepository.java
+++ BackEndDefinitivo/nuhcorre/src/main/java/com/nuhcorre/nuhcorre/repository/VagaRepository.java
@@ -4,6 +4,11 @@ import com.nuhcorre.nuhcorre.model.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
+
+    Vaga findByTitulo(String titulo);
+    Vaga findAllByEmpresaId(Long id);
+
 }
