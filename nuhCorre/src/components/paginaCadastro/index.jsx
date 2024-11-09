27bdc0e555f135a3e9564c71a08da03f14ac02fa
najort1 <calopsita443@gmail.com>
2024-11-09 20:35:01 -0300

    Fix header

--- nuhCorre/src/components/paginaCadastro/index.jsx
+++ nuhCorre/src/components/paginaCadastro/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './cadastro.css';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
