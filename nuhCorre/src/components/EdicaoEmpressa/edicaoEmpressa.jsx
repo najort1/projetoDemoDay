aa8c25348dea4571cff7214827f19847ca577ff6
Luana <luana_dark34@hotmail.com>
2024-11-18 00:38:18 -0300

    Caixa Editar nome e ícones mudados

--- nuhCorre/src/components/EdicaoEmpressa/edicaoEmpressa.jsx
+++ nuhCorre/src/components/EdicaoEmpressa/edicaoEmpressa.jsx
@@ -1,138 +1,150 @@
-import Footer from "../footer/Footer"
-import Header from "../header/Header"
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
 import './empressaEdicao.css';
+import React, { useState } from 'react';
 
-export function EdicaoEmpressa(){
+export function EdicaoEmpressa() {
+    const [isEditing, setIsEditing] = useState(false); // Estado para controlar se estamos no modo de edição
+    const [nome, setNome] = useState('Cachorro chupetão'); // Estado para o nome
+    const [profissao, setProfissao] = useState('chupar chupeta'); // Estado para a profissão
 
-    return(
+    // Função chamada quando o botão "editar" é clicado
+    function editNome() {
+        setIsEditing(true); // Ativa o modo de edição
+    }
 
+    // Função chamada quando o botão "Salvar" é clicado
+    function altSub(e) {
+        e.preventDefault(); // Previne o comportamento padrão do formulário
+        setIsEditing(false); // Desativa o modo de edição após salvar
+    }
+
+    // Função chamada quando o botão "Cancelar" é clicado
+    function altCancel() {
+        setIsEditing(false); // Desativa o modo de edição após cancelar
+    }
+
+    return (
         <>
-        
-            <Header/>
+            <Header />
 
-                <section className="perfil">
-
-                    <div className="alinhamentoTitulo">
-
-                        <div>
-
-                            <img src='https://i.pinimg.com/280x280_RS/e5/1d/04/e51d04c78c6c26bfa0ca7d7bb94af786.jpg' id='perfil' alt='perfilImg'/>
-
-                        </div>
-                        <div className="tituloUsuario">
-
-                            <h2>Cachorro chupetão</h2>
-                            <h3>chupar chupeta</h3>
-
-                        </div>
-
+            <section className="perfil">
+                <div className="alinhamentoTitulo">
+                    <div>
+                        <img src='https://i.pinimg.com/280x280_RS/e5/1d/04/e51d04c78c6c26bfa0ca7d7bb94af786.jpg' id='perfil' alt='perfilImg'/>
+                        <button className="editImagem"><box-icon name='edit'></box-icon></button>
                     </div>
+                    <div className="tituloUsuario">
+                        {/* Exibe o nome e profissão ou a caixa de edição */}
+                        {isEditing ? (
+                            <div className='editBoxNome'>
+                                <form onSubmit={altSub}>
+                                    <label htmlFor='nome'>Nome</label>
+                                    <input 
+                                        type='text' 
+                                        name='nome' 
+                                        value={nome} 
+                                        onChange={(e) => setNome(e.target.value)} // Atualiza o nome no estado
+                                    />
 
-                </section>
-                <section id='formEmpresa'>
+                                    <label htmlFor='profissao'>Profissão</label>
+                                    <input 
+                                        type='text' 
+                                        name='profissao' 
+                                        value={profissao} 
+                                        onChange={(e) => setProfissao(e.target.value)} // Atualiza a profissão no estado
+                                    />
 
-                    <nav className="linksInterno">
-
-                        <ul>
-
-                            <li>
-                                <img class='listImg'width="25" height="25" src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/64/external-home-contact-flatart-icons-outline-flatarticons-1.png" alt="external-home-contact-flatart-icons-outline-flatarticons-1"/>
-                                Página Inicial
-                            </li>
-                            <li className="linkSelecionado">
-                                <img class='listImg'width="25" height="25" src="https://img.icons8.com/deco/48/engineering.png" alt="engineering"/>
-                                Informações Pessoais</li>
-                            <li>
-                                <img class='listImg'width="25" height="25" src="https://img.icons8.com/pixels/32/conference-call.png" alt="conference-call"/>
-                                Perfil</li>
-                            <li>
-                                <img class='listImg'width="25" height="25" src="https://img.icons8.com/android/24/news.png" alt="news"/>
-                                Vagas</li>
-
-                        </ul>
-
-
-                    </nav>
-                    <div className='caixasCentrais'>
-
-                        <div className="textoLei">Seus dados pessoais estarão protegidos, nos termos da Lei 13.460/2017.</div>
-                        <div className="caixaInterna">
-
-                            <h1>Informaçoes Pessoais  </h1>
-                            <form>
-
-                                <div>
-
-                                    <label for=''>Email corporativo</label>
-                                    <input type='email'/>
-
-                                    <label for=''>CNPJ</label>
-                                    <input type='text'/>
-
-                                    <label for=''>CEP</label>
-                                    <input type='text'/>
-
-                                    <label for=''>Estado</label>
-                                    <select>
-                                        <option value="" defaultChecked>Selecione seu estado</option>
-                                    </select>
-
-                                    <label for=''>Setor de atuação</label>
-                                    <select>
-                                        <option value="" defaultChecked>Alimentício</option>
-                                    </select>
-                                    <div style={{fontWeight:600}}>
-
-                                        <label>A empresa possui politicas de inclusão?</label>
-                                        <input type='radio'/> Não
-                                        <input type='radio' style={{ marginLeft: '40px' }}/> Sim
-                                        
+                                    <div className='botoes'>
+                                        <input type='submit' className='submitCancel' value='Salvar alterações' />
+                                        <input type='button' onClick={altCancel} className='submitCancel' value='Cancelar' />
                                     </div>
-
-                                    <div id='botoes'>
-
-                                        <input type='submit' value='Salvar informações'/>
-                                        <input type='reset' value='Limpar campos'/>
-
-                                    </div>
-                                    
-
-                                </div>
-                                <div>
-
-                                    <label for=''>Senha</label>
-                                    <input type='password'/>
-
-                                    <label for=''>Telefone</label>
-                                    <input type='tel'/>
-
-                                    <label for=''>Logradouro</label>
-                                    <input type='text'/>
-
-                                    <label for=''>Cidade</label>
-                                    <select>
-                                        <option value="" defaultChecked>Selecione seu Cidade</option>
-                                    </select>
-                                    
-                                    <label for=''>Descrição da Empresa</label>
-                                    <textarea cols={69} rows={10}></textarea>
-
-                                </div>
+                                </form>
+                            </div>
+                        ) : (
+                            <>
+                                <h2 className="editNome">{nome}</h2>
+                                <button onClick={editNome}><box-icon name='edit'></box-icon></button>
+                                <h3>{profissao}</h3>
                                 
-
-                            </form>
-
-                        </div>
-
+                            </>
+                        )}
                     </div>
+                </div>
+            </section>
 
-                </section>
+            <section id='formEmpresa'>
+                <nav className="linksInterno">
+                    <ul>
+                        <li><box-icon name='home'></box-icon> Página Inicial</li>
+                        <li className="linkSelecionado"><box-icon name='cog'></box-icon> Informações Pessoais</li>
+                        <li><img className='listImg' width="25" height="25" src="https://img.icons8.com/pixels/32/conference-call.png" alt="conference-call"/> Perfil</li>
+                        <li><img className='listImg' width="25" height="25" src="https://img.icons8.com/android/24/news.png" alt="news"/> Vagas</li>
+                    </ul>
+                </nav>
 
-            
-        
+                <div className='caixasCentrais'>
+                    <div className="textoLei">Seus dados pessoais estarão protegidos, nos termos da Lei 13.460/2017.</div>
+                    <div className="caixaInterna">
+                        <h1>Informaçoes Pessoais</h1>
+
+                        {/* Formulário de edição de informações */}
+                        <form>
+                            <div>
+                                <label htmlFor='email'>Email corporativo</label>
+                                <input type='email' name='email' />
+
+                                <label htmlFor='cnpj'>CNPJ</label>
+                                <input type='text' name='cnpj' />
+
+                                <label htmlFor='cep'>CEP</label>
+                                <input type='text' name='cep' />
+
+                                <label htmlFor='estado'>Estado</label>
+                                <select name='estado'>
+                                    <option value="" defaultChecked>Selecione seu estado</option>
+                                </select>
+
+                                <label htmlFor='setor'>Setor de atuação</label>
+                                <select name='setor'>
+                                    <option value="" defaultChecked>Alimentício</option>
+                                </select>
+
+                                <div style={{ fontWeight: 600 }}>
+                                    <label>A empresa possui políticas de inclusão?</label>
+                                    <input type='radio' name='inclusao' /> Não
+                                    <input type='radio' name='inclusao' style={{ marginLeft: '40px' }} /> Sim
+                                </div>
+
+                                <div id='botoes'>
+                                    <input type='submit' value='Salvar informações' />
+                                    <input type='reset' value='Limpar campos' />
+                                </div>
+                            </div>
+
+                            <div>
+                                <label htmlFor='senha'>Senha</label>
+                                <input type='password' name='senha' />
+
+                                <label htmlFor='telefone'>Telefone</label>
+                                <input type='tel' name='telefone' />
+
+                                <label htmlFor='logradouro'>Logradouro</label>
+                                <input type='text' name='logradouro' />
+
+                                <label htmlFor='cidade'>Cidade</label>
+                                <select name='cidade'>
+                                    <option value="" defaultChecked>Selecione sua Cidade</option>
+                                </select>
+
+                                <label htmlFor='descricao'>Descrição da Empresa</label>
+                                <textarea name='descricao' cols={55} rows={10}></textarea>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+
         </>
-
-
-    )
-
+    );
 }
